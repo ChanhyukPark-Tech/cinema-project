18056f8b290d8c4c9a68f76a6fb780812ecceeed
Chanhyuk Park <pgg6713@gmail.com>
2021-11-08 02:23:42 +0900

    프론트 컨택트페이지작업했지만 브랜치는 back 입니다

--- client/src/pages/branchPage/BranchPage.js
+++ client/src/pages/branchPage/BranchPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from "../../components/header/Header";
 import BranchSelection from "../../components/BranchSelection/BranchSelection";
-import {useParams} from "react-router-dom";
 
 function BranchPage(props) {
     return (
