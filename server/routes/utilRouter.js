c0edcfda6bfc351277d055bd2e3defd37f4ae132
Chanhyuk Park <pgg6713@gmail.com>
2021-11-08 01:30:01 +0900

    백엔드 지점기능 구현

--- server/routes/utilRouter.js
+++ server/routes/utilRouter.js
@@ -30,19 +30,41 @@ const router = require("express").Router();
 
  /**
  * @swagger
- *  /api/util/Divison:
+ *  /api/util/divison:
  *    get:
  *      tags:
  *      - util
  *      description: 모든 지역 보내주기 제공
  */
 
+/**
+ * @swagger
+ *  /api/util/place:
+ *    post:
+ *      tags:
+ *      - util
+ *      description: 한 지역에 해당하는 모든 지점 보내주기
+ */
+
+
+/**
+ * @swagger
+ *  /api/util/placeDetail:
+ *    post:
+ *      tags:
+ *      - util
+ *      description: 한 지점에 해당하는 상세정보 보여주기
+ */
+
 
 router.route('/place')
     .get(utilCtrl.getPlaces)
+    .post(utilCtrl.getPlace)
 
 router.route('/divison')
     .get(utilCtrl.getDivisons)
 
+router.route('/placeDetail')
+    .post(utilCtrl.getSpecifyPlace)
 
 module.exports = router;
