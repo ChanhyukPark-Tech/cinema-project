38c60e61c1ec2ddcefb6c12cdd2cf3e48484ea68
JUNG EUNJIN <jayjinnie@hanyang.ac.kr>
2021-11-20 21:07:06 +0900

    layout 설정
    새싹마켓 페이지와 유사한 틀로 구현
    준수오빠 pr merge해서 propsCtrl 받아옴

--- client/src/pages/adminPages/facilityPage/FacilityPage.js
+++ client/src/pages/adminPages/facilityPage/FacilityPage.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Header from "../../../components/header/Header";
+import Footer from "../../../components/Footer/Footer";
+import { Link } from "react-router-dom";
+import { Button, Table, Tag } from "antd";
+import Title from "../../../components/Title/Title";
+import axios from "axios";
 
 function FacilityPage(props) {
-    
+    const [posts, setPosts] = useState([]);
+    const lists = [
+        {
+            title: "날짜",
+            dataIndex: "date",
+            key: "date",
+        },
+        {
+            title: "제목",
+            dataIndex: "title",
+            key: "title",
+            render: (text, index) => {
+                return (
+                    <Link to={`/props`}>
+
+                    </Link>
+                )
+            }
+        }
+    ]
 
     return (
         <div>시설관리</div>
---
+++ client/src/pages/adminPages/facilityPage/PostDamagePage.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import Header from "../../../components/header/Header";
+import Footer from "../../../components/Footer/Footer";
+import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
+import axios from 'axios';
+import {Select} from "antd";
+
+const {Option} = Select;
+
+function FacilityPage(props) {
+    
+
+    return (
+        <div>시설관리</div>
+    );
+}
+
+export default FacilityPage;
