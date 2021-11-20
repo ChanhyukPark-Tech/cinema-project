b6798b37a1d8d8c242dc8e26f5a70c096e01a4f1
junsusong98 <sb020578@naver.com>
2021-11-20 20:43:31 +0900

    props

---
+++ server/controllers/propsCtrl.js
@@ -0,0 +1,22 @@
+const connection = require('../dbConfig')
+
+const propsCtrl = {
+    getprops : async (req,res)=>{
+        connection.query(`SELECT * FROM event` ,(error,rows)=>{
+            if(error) throw error;
+            res.send(rows);
+        })
+    },
+
+    // getSpecifyEvent : async (req,res)=>{
+    //     const {event_id} = req.body;
+    //     const sql = `SELECT * FROM event where event_id = '${event_id}'`
+    //     connection.query(
+    //         sql,(error,rows) => {
+    //             if(error) throw error;
+    //             res.send(rows);
+    //         }
+    //     )
+    // }
+}
+module.exports = propsCtrl
