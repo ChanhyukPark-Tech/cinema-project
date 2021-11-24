5ad6d946fc5ed2ffb2c2b7d4924bb0107185ca21
junsusong98 <sb020578@naver.com>
2021-11-24 18:21:28 +0900

    movieReview

--- server/controllers/movieCtrl.js
+++ server/controllers/movieCtrl.js
@@ -32,6 +32,61 @@ const movieCtrl = {
                 res.send(rows);
             }
         )
+    },
+
+    reviewMovies : async (req,res) => {
+        // javascript 구조분해할당
+        const {RepresentationMovieCode} = req.body;
+        const sql = `SELECT * FROM review WHERE movie_RepresentationMovieCode = "${RepresentationMovieCode}";`
+        connection.query(
+            sql,(error,rows) => {
+                if(error) throw error;
+                res.send(rows);
+            }
+        )
+    },
+    
+    reviewMovieDelete : async (req,res) => {
+        // javascript 구조분해할당
+        const {RepresentationMovieCode, member_id} = req.body;
+        const sql = `DELETE FROM review WHERE movie_RepresentationMovieCode = "${RepresentationMovieCode}" AND member_member_id = ${member_id};`
+        connection.query(
+            sql,(error,rows) => {
+                if(error) throw error;
+                res.send(rows);
+            }
+        )
+    },
+
+    reviewMovieRatingUpdate: async (req, res) => {
+        const {
+            
+            RepresentationMovieCode,
+            member_id,
+            content,
+            regDt,
+            rating
+
+        } = req.body;
+
+        const sql1 = ` INSERT INTO review VALUES("${RepresentationMovieCode}", ${member_id}, "${content}", "${regDt}", ${rating});`;
+        connection.query(sql1, (error, rows) => {
+            if (error) throw error;
+        });
+
+        const sq3 = `set sql_safe_updates=0;`
+        connection.query(
+            sq3,(error,rows) => {
+                if(error) throw error;
+                res.send(rows);
+            }
+        )
+
+        const sql2 = `UPDATE movie SET rate = (SELECT round(avg(rating),1) FROM review WHERE movie_RepresentationMovieCode = "${RepresentationMovieCode}")
+        WHERE RepresentationMovieCode = "${RepresentationMovieCode}";`;
+        connection.query(sql2, (error, rows) => {
+            if (error) throw error;
+        });
     }
 
 }
