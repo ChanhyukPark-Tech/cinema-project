655811c739b3937f333166bc918cb43f0c3d4e5f
Chanhyuk Park <pgg6713@gmail.com>
2021-11-28 20:57:32 +0900

    myplace

--- client/src/pages/adminPages/salesPage/GenderTotalPay.js
+++ client/src/pages/adminPages/salesPage/GenderTotalPay.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {myPlaceGenderTotalPay} from "../adminMainPage/chartConfig";
 import {Bar} from "react-chartjs-2";
+import sessack from "../../marketPage/sproutChar.gif";
+import sessackPink from "../../marketPage/sproutCharPink.gif";
 
 function GenderTotalPay(props) {
 
@@ -16,13 +18,24 @@ function GenderTotalPay(props) {
 
     const result = myPlaceGenderTotalPay(["여성", "남성"], [genderTotal.woman, genderTotal.man])
 
+
+    const imagestyle = {
+        height: "15vh",
+        width: "15vw",
+    };
+
     return (
-        <div style={{width: '60%'}}>
+        <div style={{width: '80%', position: 'relative' ,margin:'0 auto'}}>
 
             <Bar
                 data={result}
             />
-
+            <span style={{position:'absolute',top:'24vh',right:'10vw'}}>
+                <img src={sessack} style={imagestyle} alt={sessack}/>
+              </span>
+            <span style={{position:'absolute',top:'0vh',left:'15vw'}}>
+                <img src={sessackPink} style={imagestyle} alt={sessack}/>
+              </span>
         </div>
     );
 }
